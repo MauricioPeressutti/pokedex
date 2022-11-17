f6fde81415f8c75fc77cdadb84221e42e9bb4720
Martín Gesualdo <mgesudev@gmail.com>
2022-11-17 20:17:07 -0300

    Cambios

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
   const [loading, setLoading] = useState("")
 
   const session = useSession()
-  console.log({ session })
 
   const handleChange = ({ target }) =>
     setPokemonName(target.value.toLowerCase())
