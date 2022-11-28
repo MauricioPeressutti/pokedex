9ad441e887bdb5347722bb3e0ee3831e1ae8d3c2
Martín Gesualdo <mgesudev@gmail.com>
2022-11-27 23:24:17 -0300

    Cambios

--- pages/api/suma.js
+++ pages/api/suma.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  console.log({ body: req.body })
   const { num1, num2 } = JSON.parse(req.body)
   console.log({ num1, num2 })
   return num1 + num2
